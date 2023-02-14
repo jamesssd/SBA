536c748bae6e2d047c308f64fc925e3670989a6b
James Dizon <dizonjerlin@gmail.com>
2023-02-14 13:05:32 -0800

    Updated Files

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
+// import config from "../config,js"
+
 var topics = ["Full House", "Grey's Anatomy", "How to Get Away with Murder", "One Tree Hill", "Gossip Girl", "The Good Doctor", "Pokemon", "Naruto"];
 var numberOfGIFs = 15;
 var cutOffRating = "PG-13";
-var API = config.API;
+// var API = config.API;
 
 function renderButtons(){
 	for(var i = 0; i < topics.length; i++) {
@@ -33,7 +35,7 @@ function addButton(show){
 function populateGIFContainer(show){
 	$.ajax({
 		url: "https://api.giphy.com/v1/gifs/search?q=" + show + 
-		API + cutOffRating + "&limit=" + numberOfGIFs,
+		"&api_key=61TKqzUDPHfv40Bqr6iEsqqBCfa360mt&rating=" + cutOffRating + "&limit=" + numberOfGIFs,
 		method: "GET"
 	}).then(function(response){
 		response.data.forEach(function(element){
@@ -72,3 +74,33 @@ $(document).ready(function(){
 		$("#tv-show").val("");
 	});
 });
+
+function validateForm() {
+    var name =  document.getElementById('name').value;
+    if (name == "") {
+        document.querySelector('.status').innerHTML = "Name cannot be empty";
+        return false;
+    }
+    var email =  document.getElementById('email').value;
+    if (email == "") {
+        document.querySelector('.status').innerHTML = "Email cannot be empty";
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            document.querySelector('.status').innerHTML = "Email format invalid";
+            return false;
+        }
+    }
+    var subject =  document.getElementById('subject').value;
+    if (subject == "") {
+        document.querySelector('.status').innerHTML = "Subject cannot be empty";
+        return false;
+    }
+    var message =  document.getElementById('message').value;
+    if (message == "") {
+        document.querySelector('.status').innerHTML = "Message cannot be empty";
+        return false;
+    }
+    document.querySelector('.status').innerHTML = "Sending...";
+  }
